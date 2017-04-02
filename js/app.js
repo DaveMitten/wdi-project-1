1df0a44d8b8d240322b8aaff9f40974f824c914e
DaveMitten <davidmitten88@gmail.com>
2017-04-02 14:23:56 +0100

    changed speed div moves at

--- js/app.js
+++ js/app.js
@@ -2,10 +2,28 @@
 $(document).ready(function(){
 
 
-// console.log(game);
-$('button').click(function startDescent() {
-  $('.note').animate({'top': '+=700px'});
+//if button is clicked, perform action. Keep true untill the div reaches its targe of 700 px, when that happens, remove class.
+
+//then adde class at top and start sequence again
+
+// const top = $('.note')
+
+  $('button').click(function (){
+    $('.note').animate({'top': '+=700px'}, 3000);
+
+  });
+// if('.note' === 'top' ))
+// .removeClass('.note');
+
+
+
+
+
+
+
+
+
   // console.log('startDescent');
-});
+
 
 });
