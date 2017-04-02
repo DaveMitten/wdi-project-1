fcbf6a25edcf4c3f81bddfb25cc11d1d8622366c
DaveMitten <davidmitten88@gmail.com>
2017-04-02 16:38:54 +0100

    tried to add a device to tell position value

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ needs to a div in that collumn
 
 needs a div at the bottom of said collumn
 
+
+
+
 ...
 
 attempt to animate the html element note to get it to slide down the runway.
@@ -20,6 +23,14 @@ select the element that i want animate
 to start th animation i need an event listener to be activated.
 ...
 
+
+remove div when it reaches the bottom of the collumn (at 700 px)
+
+remove() - Removes the selected element (and its child elements)
+empty() - Removes the child elements from the selected element
+might be using these.
+
+attempting to use offset to produce the coordinates for my div so that i can use this to add a 'removeclass' to my div
 
 
 
--- js/app.js
+++ js/app.js
@@ -2,18 +2,47 @@
 $(document).ready(function(){
 
 
-//if button is clicked, perform action. Keep true untill the div reaches its targe of 700 px, when that happens, remove class.
+  //if button is clicked, perform action. Keep true untill the div reaches its targe of 700 px, when that happens, remove class.
 
-//then adde class at top and start sequence again
+  //then adde class at top and start sequence again
 
-// const top = $('.note')
+  // const top = $('.note')
 
-  $('button').click(function (){
-    $('.note').animate({'top': '+=700px'}, 3000);
 
+  const buttons = $('.buttons');
+  const note = $('.note');
+  // const button = $('button');
+  // const noteTop = note.top(700px;)
+
+
+  buttons.click(function (){
+    note.animate({'top': '+=700px'}, 3000);
+    let x = note.offset();
+    return(x.top);
+
+//     let noteTop = note.postion.top
+//
+//     $(document).scroll(function() {
+//   var y = $(this).scrollTop();
+//   if (y > 800) {
+//     $('.bottomMenu').fadeIn();
+//   } else {
+//     $('.bottomMenu').fadeOut();
+//   }
+// });
+
+
+
+
+    // note.removeClass();
+    // if()
   });
-// if('.note' === 'top' ))
-// .removeClass('.note');
+
+  // if(note === top = 700px){
+  //   note.removeClass();
+
+  // }
+
 
 
 
