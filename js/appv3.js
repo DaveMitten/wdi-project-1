6868d5da7fc44f38eb30ed29b40adbe46aa7a790
DaveMitten <davidmitten88@gmail.com>
2017-04-05 11:46:39 +0100

    notes now work with keypress

--- js/appv3.js
+++ js/appv3.js
@@ -7,13 +7,14 @@ Game.init = function(){
   Game.playerScore  = 0;
   Game.playerLives  = 5;
   Game.difficulty   = 900;
-  Game.speed        = 2000;
+  Game.speed        = 10000;
   Game.score        = $('.scoreBoard').html(`Score: ${Game.playerScore}`);
 
   Game.$start.on('click', Game.startGame);
 };
 
 Game.startGame = function(){
+  Game.keyPress();
   Game.interval = setTimeout(Game.$createNote, Game.difficulty);
 };
 
@@ -23,9 +24,10 @@ Game.chooseRandomRunway = function() {
 
 Game.$createNote = function (){
   Game.interval = setTimeout(Game.$createNote, Game.difficulty);
-  Game.$note = $('<div class="note"></div>');
-  $(Game.chooseRandomRunway()).append(Game.$note);
-  Game.animate(Game.$note);
+  const $runway = Game.chooseRandomRunway();
+  Game.$note = $(`<div class="note" id="${$runway.attr('id')}"></div>`);
+  $($runway).append(Game.$note);
+  Game.animate();
 };
 
 Game.animate = function(){
@@ -46,13 +48,87 @@ Game.positionCheck = function() {
   if( $(this).position().top > 800 ) $(this).stop().remove();
 };
 
-Game.playerClick = Game.$end.click(function clickNote() {
-  const notePos = $('.note').position();
-  if(notePos.top >= 600 && notePos.top <= 810 ){
-    $('.note').eq(0).remove();
-    Game.playerScore++;
-    Game.score = $('.scoreBoard').html(`Score: ${Game.playerScore}`);
+// Game.playerClick = Game.$end.click(function clickNote() {
+//   const notePos = $('.note').position();
+//   if(notePos.top >= 600 && notePos.top <= 810 ){
+//     $('.note').eq(0).remove();
+//     Game.playerScore++;
+//     Game.score = $('.scoreBoard').html(`Score: ${Game.playerScore}`);
+//   }
+// });
+
+Game.checkForNotes = function(keyCode) {
+  // console.log($(`#${keyCode}`));
+  const $keyRunway = $(`#${keyCode}`);
+  const $lastNoteInRunway = $keyRunway.children().first();
+  let notePosition;
+
+  if ($lastNoteInRunway.length !== 0) {
+    notePosition = $($lastNoteInRunway).position();
+
+    if(notePosition.top >= 600 && notePosition.top <= 810 ){
+      $($lastNoteInRunway).stop().remove();
+      // Game.playerScore++;
+      // Game.score = $('.scoreBoard').html(`Score: ${Game.playerScore}`);
+      console.log('correctHit');
+    } else {
+      console.log('wrongHit');
+    }
   }
-});
+
+
+
+  // $.each($notesInRunway, function(index, note) {
+  //   const notePosition = $(note).position();
+  //   if(notePosition.top >= 600 && notePosition.top <= 810 ){
+  //     $(note).stop().remove();
+  //     Game.playerScore++;
+  //     Game.score = $('.scoreBoard').html(`Score: ${Game.playerScore}`);
+  //     console.log('correctHit');
+  //   } else {
+  //     console.log('wrongHit');
+  //   }
+  // });
+
+
+  // console.log($keyRunway.children()); // -> array of notes within this runway
+
+  // loop over all notes in runway and check if any note in within the correct place to remove it
+  // notePos.top >= 600 && notePos.top <= 810 -> user has clicked correct key
+  //
+};
+
+Game.keyPress = function() {
+  $(document).on('keydown', function(e) {
+
+    switch (e.which) {
+      case 90:
+      // console.log('Z');
+      Game.checkForNotes(90);
+      break;
+      case 88:
+      // console.log('X');
+      Game.checkForNotes(88);
+      break;
+      case 67:
+      // console.log('C');
+      Game.checkForNotes(67);
+      break;
+      case 86:
+      // console.log('V');
+      Game.checkForNotes(86);
+      break;
+      case 66:
+      // console.log('B');
+      Game.checkForNotes(66);
+      break;
+    }
+  });
+};
 
 $(Game.init);
+
+//
+// z == 64
+//
+// <div class='note' id='64'></div>
